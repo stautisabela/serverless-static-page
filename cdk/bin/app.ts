c73d3950c65b66cb3a8aa0207c71dabdeabb0a25
Isabela Staut <stautisabela@gmail.com>
2026-03-29 11:21:20 +0200

    remove unwanted extension

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
-import { S3WebsiteStack } from '../lib/s3-website-stack.ts'
+import { S3WebsiteStack } from '../lib/s3-website-stack'
 const app = new cdk.App();
 new S3WebsiteStack(app, 'S3WebsiteStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
